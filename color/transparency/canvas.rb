b24eb5f2351af522ab02176f21ed0901f44906d9
Kato Atsue <atue.kato@gmail.com>
2013-11-12 11:58:41 +0900

    fix encoding

--- color/transparency/canvas.rb
+++ color/transparency/canvas.rb
@@ -8,7 +8,7 @@ class Canvas
 
   def canvas
 		<<-"EOS"
-<?xml version="1.0" standalone="no"?>
+<?xml version="1.0" encoding="utf-8" standalone="no"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" 
  "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 <svg version="1.1"
