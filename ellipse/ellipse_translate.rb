3ac56c50b51a1eba00318af5645c67fbf1b26b54
Kato Atsue <atue.kato@gmail.com>
2013-11-14 15:40:42 +0900

    fix ellipse

--- ellipse/ellipse_translate.rb
+++ ellipse/ellipse_translate.rb
@@ -1,22 +1,15 @@
 
-x = 250
-y = 220
+require_relative '../lib/svg_canvas'
 
-f = open("ellipse_translate.svg", "w") 
-f.write(<<"EOS"
-<?xml version="1.0" encoding="utf-8" standalone="no"?>
-<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" 
-  "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-<svg version="1.1"
-  xmlns="http://www.w3.org/2000/svg"
-  xmlns:xlink="http://www.w3.org/1999/xlink"
-  width="#{x}" height="#{y}"
-  viewBox="0 0 #{x} #{y}">
-EOS
-)
+# header
+s = SvgCanvas.new(250, 220, "ellipse_translate")
+f = s.header
+
+# main
 0.step(177,3){|i|
   f.write('<ellipse transform="translate(' + i.to_s + ' 0)" fill="none" stroke="#888"
   cx="40" cy="110" rx="30" ry="100" stroke-width="0.3" />' + "\n")
 }
-f.write("</svg>\n")
-f.close
+
+# footer
+s.footer
