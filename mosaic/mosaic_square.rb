42726a7310e2dfe839eac69d05f0dfb43b4f8da4
Kato Atsue <atue.kato@gmail.com>
2013-11-14 16:31:14 +0900

    fix mosaic

--- mosaic/mosaic_square.rb
+++ mosaic/mosaic_square.rb
@@ -1,21 +1,13 @@
 
-x = 1200
-y = 1000
-f = open("mosaic_square.svg", "w") 
-f.write(<<"EOS"
-<?xml version="1.0" encoding="utf-8" standalone="no"?>
-<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" 
- "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-<svg version="1.1"
-  xmlns="http://www.w3.org/2000/svg"
-  xmlns:xlink="http://www.w3.org/1999/xlink"
-  width="#{x}" height="#{y}"
-  viewBox="0 0 #{x} #{y}">
-<title>mosaic_square</title>
-EOS
-)
-0.step(x,10){|i|
-  0.step(y,10){|j|
+require_relative '../lib/svg_canvas'
+
+# header
+s = SvgCanvas.new(1200, 1000, "mosaic_square")
+f = s.header
+
+# main
+0.step(1200,10){|i|
+  0.step(1000,10){|j|
     ratio = [0.85, 0.82, 0.8, 0.78, 0.75, 0.73, 0.7, 0.5, 0.3, 0].sample
     r = rand(160..220)
     g = rand(15) + r + 20
@@ -24,5 +16,6 @@ EOS
     f.write %(<rect x="#{i.to_s}" y="#{j.to_s}" fill="#{rgb}" width="9" height="9" />\n)
   }
 }
-f.write %(</svg>\n)
-f.close
+
+# footer
+s.footer
