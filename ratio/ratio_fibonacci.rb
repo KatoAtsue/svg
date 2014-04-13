d689e5efe04c5f56cd5b628edc0a256d69576c8d
Kato Atsue <atue.kato@gmail.com>
2014-04-13 20:34:42 +0900

    fix ratio

--- ratio/ratio_fibonacci.rb
+++ ratio/ratio_fibonacci.rb
@@ -11,32 +11,29 @@ def fib(n)
   end
 end
 
-
 # header
-s = SvgCanvas.new(1500, 1500, "ratio_fibonacci")
+s = SvgCanvas.new(1200, 800, "ratio_fibonacci")
 f = s.header
 
 # main
 add_sub = [[-1,1,1,-1],[-1,-1,1,1],[1,-1,-1,1],[1,1,-1,-1]]
-rot = 15
+rot = 22
 
-f.write %(<g stroke="#666" stroke-width="0.5" fill="#aaf" fill-opacity="0.1">\n)
-17.times{|i|
-  mx = 500
-  my = 500
+f.write %(<g stroke="#666" stroke-width="0.3" fill="none">\n)
+58.times{|i|
+  mx = 360
+  my = 360
   ra = []
   rot.times{|n| ra << fib(n + 1)}
-  ra.map!{|n| n * 0.05 * (20 - i)}
-
+  ra.map!{|n| n * 0.002 * (68 - i)}
   rot.times{|j|
     as = add_sub[j % 4]
     mx += ra[j] * as[0]
     my += ra[j] * as[1]
-    f.write %(<path d="M#{mx},#{my} a#{ra[j]},#{ra[j]} 0 0,0 #{ra[j] * as[2]},#{ra[j] * as[3]} z" />\n)
+    f.write %(<path d="M#{"%0.2f" % mx},#{"%0.2f" % my} a#{"%0.2f" % ra[j]},#{"%0.2f" % ra[j]} 0 0,0 #{"%0.2f" % (ra[j] * as[2])},#{"%0.2f" % (ra[j] * as[3])} z" />\n)
   }
 }
 f.write %(</g>\n)
-
 
 # footer
 s.footer
