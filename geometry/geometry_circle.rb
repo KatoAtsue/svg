2a235953cee823ab7365503d15fb2b74cd975598
Kato Atsue <atue.kato@gmail.com>
2014-06-12 20:14:53 +0900

    fix geometry

--- geometry/geometry_circle.rb
+++ geometry/geometry_circle.rb
@@ -10,40 +10,39 @@ f = s.header
 cn = 200
 3.times{|i|
   3.times{|j|
-    10.times{|k|
-      dash = ['stroke="#999" stroke-dasharray="2" ','stroke="#aaa" '].sample
-      f.write('<g fill="none" stroke-width="0.3" ' + dash + ">\n")
-      cx = (i * 2 + 1) * cn
-      cy = (j * 2 + 1) * cn
-      cs = rand(5)
-      l1 = rand(1..6) * 10
-      l2 = rand(10..18) * 10
-      l3 = rand(5..10) * 10
-      rd = [30, 20, 18, 15, 10, 6, 4, 2].sample
-      rx = (rand(1..7) * 10).to_s
-      ry = (rand(1..7) * 10).to_s
-      co = [
-	      [[rd, l1]],
-  	    [[rd, l1]],
-    	  [[0, l3],[rd, l1]],
-      	[[rd / 2, l3], [0, l2], [-rd / 2, l3], [rd, l1]],
-      	[[rd / 2, l3], [rd / 2, l2], [-rd / 2, l2], [-rd / 2, l3], [rd, l1]]
-      ]
-      mx = (cx - cos(PI * (90 - rd) / 180) * l1).to_s
-      my = (cy - sin(PI * (90 - rd) / 180) * l1).to_s
-      f.write('<path d="M ' + mx + ',' + my + "\n")
-      (180 / rd).times{|m|
-        co[cs].each {|n|
-          ax = (cx + cos(PI * (90 - n[0] - m * rd * 2) / 180) * n[1]).to_s
-          ay = (cy - sin(PI * (90 - n[0] - m * rd * 2) / 180) * n[1]).to_s
-          if cs == 0
-            f.write('A' + rx + ',' + ry + ' ' + (m * rd * 2).to_s + ' 1,1 ' + ax + ',' + ay + "\n")
-          else
-            f.write('L' + ax + ',' + ay + "\n")
-          end
-        }
+    x = (i * cn * 2) + cn
+    y = (j * cn * 2) + cn
+    xs = x.to_s
+    ys = y.to_s
+    f.write %(<circle fill="#aaa" stroke="none" cx="#{xs}" cy="#{ys}" r="1" />\n)
+    18.times{|k|
+      r1 = (rand(k + 1)) * 0.7 + 1
+      r2 = (rand(k + 1)) * 0.7 + 1
+      rd1 = r1 * 2
+      rd2 = r2 * 2
+      rh1 = (x - r1).to_s
+      rh2 = (x + r1).to_s
+      rs1 = r1.to_s
+      rs2 = r2.to_s
+      iv = y - 5 - (k * 10)
+      s1 = %(circle cx="#{xs}" cy="#{iv.to_s}" r="#{rs1}" )
+      s2 = %(ellipse cx="#{xs}" cy="#{iv.to_s}" rx="#{rs1}" ry="#{rs2}" )
+      s3 = %(polygon points="#{xs} #{(iv - r1).to_s},#{rh1} #{(iv + r2).to_s},#{rh2} #{(iv + r2).to_s}" )
+      s4 = %(polygon points="#{xs} #{(iv - r2).to_s},#{rh1} #{iv.to_s},#{xs} #{(iv + r2).to_s},#{rh2} #{iv.to_s}" )
+      s5 = %(rect x="#{rh1}" y="#{(iv - r2).to_s}" width="#{rd1.to_s}" height="#{rd2.to_s}" )
+      fg = [s1, s2, s3, s4, s5].sample
+      c1 = ['666', '888', 'aaa']
+      c2 = ['000', '333', '666']
+      cl1 = %(fill="##{c1.sample}" fill-opacity="0.1" stroke="none" )
+      cl2 = %(fill="none" stroke="##{c2.sample}" stroke-width="0.3" stroke-opacity="0.5" )
+      st = [cl1, cl2].sample
+      n = [40, 30, 20, 18, 10, 6, 3].sample
+      0.step(359,n){|l|
+      f.write('<')
+      f.write(fg)
+      f.write(st)
+      f.write %(transform="rotate(#{l.to_s},#{xs},#{ys})" />\n)
       }
-      f.write('" />' + "\n</g>\n")
     }
   }
 }
